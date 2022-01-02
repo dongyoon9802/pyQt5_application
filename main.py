57d10bf258fcf22bfbcbc2b34ca7b82e78751504
dongyoon9802 <dongyoon9802@gmail.com>
2022-01-02 22:19:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QSplitter, QFrame,  QDoubleSpinBox, QComboBox, QPushButton, QMainWindow, QWidget, QDesktopWidget, QLabel, QVBoxLayout, QLineEdit
+from PyQt5.QtWidgets import QApplication, QScrollArea, QSplitter, QFrame,  QDoubleSpinBox, QComboBox, QPushButton, QMainWindow, QWidget, QDesktopWidget, QLabel, QVBoxLayout, QLineEdit, QTabWidget
 from PyQt5.QtCore import QTime, Qt
 from PyQt5.QtGui import *
 
 
-class MyApp(QMainWindow):
+class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -12,55 +12,39 @@ class MyApp(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        tab = QTabWidget()
+
+        tab_1 = self.create_tab_1()
+        tab_2 = self.create_tab_2()
+
+        tab.addTab(tab_1, "tab_1")
+        tab.addTab(tab_2, "tab_2")
+
         main_layout = QVBoxLayout()
+        main_layout.addWidget(tab)
 
-        frame_1 = QFrame()
-        frame_1.setFrameShape(QFrame.Panel | QFrame.Sunken)
-        frame_2 = QFrame()
-        frame_2.setFrameShape(QFrame.Panel | QFrame.Sunken)
-        frame_3 = QFrame()
-        frame_3.setFrameShape(QFrame.Panel | QFrame.Sunken)
+        self.setLayout(main_layout)
+        self.resize(500, 500)
+        self.show()
 
+    def create_tab_1(self):
         layout_1 = QVBoxLayout()
-        layout_2 = QVBoxLayout()
-        layout_3 = QVBoxLayout()
-
-        self.statusBar().showMessage(self.dateTime.toString(Qt.DefaultLocaleLongDate))
-        self.setWindowTitle('업비트 자동매매')
-        self.resize(500, 300)
 
         # 텍스트영역
-        label1 = QLabel('API key', self)
-        label1.move(10, 10)
+        label1 = QLabel('종목선택', self)
+        label1.move(10, 55)
         label1.setAlignment(Qt.AlignVCenter)
         font1 = label1.font()
         font1.setBold(True)
         label1.setFont(font1)
 
-        label2 = QLabel('종목선택', self)
-        label2.move(10, 55)
+        label2 = QLabel('주문방식', self)
+        label2.move(10, 100)
         label2.setAlignment(Qt.AlignVCenter)
         font2 = label2.font()
         font2.setBold(True)
         label2.setFont(font2)
-
-        label3 = QLabel('주문방식', self)
-        label3.move(10, 100)
-        label3.setAlignment(Qt.AlignVCenter)
-        font3 = label3.font()
-        font3.setBold(True)
-        label3.setFont(font3)
         # 텍스트영역 끝
-
-        # 문자입력 파트
-        qle = QLineEdit(self)
-        qle.move(80, 10)
-        # 문자입력 파트 끝
-
-        # 버튼 파트
-        btn1 = QPushButton('입력', self)
-        btn1.move(200, 10)
-        # 버튼 파트 끝
 
         # 종목설정메뉴
         cb1 = QComboBox(self)
@@ -108,7 +92,7 @@ class MyApp(QMainWindow):
 
         # 슬리피지
         label6 = QLabel('슬리피지', self)
-        label6.move(200, 145)
+        #label6.move(200, 145)
         label6.setAlignment(Qt.AlignVCenter)
         font6 = label6.font()
         font6.setBold(True)
@@ -119,29 +103,59 @@ class MyApp(QMainWindow):
         dspinbox3.setRange(0, 100)
         dspinbox3.setSingleStep(0.01)
         dspinbox3.setPrefix('% ')
-        dspinbox3.move(270, 145)
+        #dspinbox3.move(270, 145)
         dspinbox3.setToolTip('신호발생 이후 몇 % 이상 높은 가격까지 주문을 허용할 것인지 설정')
         # 슬리피지 끝
 
         # widget 생성
         layout_1.addWidget(label1)
-        layout_1.addWidget(qle)
-        layout_1.addWidget(btn1)
-
-        layout_2.addWidget(label2)
-        layout_2.addWidget(label3)
-        layout_2.addWidget(cb1)
-        layout_2.addWidget(cb2)
-
-        layout_3.addWidget(label4)  # 로스컷( %)
-        layout_3.addWidget(label5)  # 익절컷( %)
-        layout_3.addWidget(label6)  # 슬리피지( %)
-        layout_3.addWidget(dspinbox1)
-        layout_3.addWidget(dspinbox2)
-        layout_3.addWidget(dspinbox3)
+        layout_1.addWidget(label2)
+        layout_1.addWidget(cb1)
+        layout_1.addWidget(cb2)
+        layout_1.addWidget(label4)  # 로스컷( %)
+        layout_1.addWidget(label5)  # 익절컷( %)
+        layout_1.addWidget(label6)  # 슬리피지( %)
+        layout_1.addWidget(dspinbox1)
+        layout_1.addWidget(dspinbox2)
+        layout_1.addWidget(dspinbox3)
         # widget 생성 끝
+        widget = QWidget()
+        widget.setLayout(layout_1)
+        scroll_area = QScrollArea()
+        scroll_area.setWidget(widget)
+        scroll_area.setWidgetResizable(True)
+        return scroll_area
 
-        self.show()
+    def create_tab_2(self):
+        layout_3 = QVBoxLayout()
+
+        label3 = QLabel('API key', self)
+        label3.move(10, 10)
+        label3.setAlignment(Qt.AlignVCenter)
+        font3 = label3.font()
+        font3.setBold(True)
+        label3.setFont(font3)
+
+        # 문자입력 파트
+        qle = QLineEdit(self)
+        qle.move(80, 10)
+        # 문자입력 파트 끝
+
+        # 버튼 파트
+        btn1 = QPushButton('입력', self)
+        btn1.move(200, 10)
+        # 버튼 파트 끝
+
+        layout_3.addWidget(label3)
+        layout_3.addWidget(qle)
+        layout_3.addWidget(btn1)
+
+        widget = QWidget()
+        widget.setLayout(layout_3)
+        scroll_area = QScrollArea()
+        scroll_area.setWidget(widget)
+        scroll_area.setWidgetResizable(True)
+        return scroll_area
 
     def center(self):
         qr = self.frameGeometry()
